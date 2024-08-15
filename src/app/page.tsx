a05f974b374f85e93cdd8f1bd76b6f9d95e805c7
Vinicius Paes Berna <viniciuspaesberna@gmail.com>
2024-08-15 12:52:30 -0300

    chore: set crossorign to anonymous

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { RedeemCodeForm } from '@/components/redeem-code-form'
 
 export default function Home() {
   return (
-    <main className="my-auto flex flex-col items-center justify-center space-y-8">
+    <main className="flex flex-col items-center justify-center space-y-8 pt-36">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-center text-2xl font-bold">Resgate seu cupom</h1>
 
