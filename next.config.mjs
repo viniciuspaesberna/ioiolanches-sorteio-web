edbd527425d5860058b198579915311b0dc29e16
Vinicius Paes Berna <viniciuspaesberna@gmail.com>
2024-08-15 12:59:02 -0300

    chore: accesscontrolalloworigin to *

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: {
-    accessControlAllowOrigin: '*',
-  },
+  headers: [
+    {
+      source: '/api/:path*',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: '*', // Set your origin
+        },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET, POST, PUT, DELETE, OPTIONS',
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value: 'Content-Type, Authorization',
+        },
+      ],
+    },
+  ],
 }
 
 export default nextConfig
